ea89203f0b9bee86f43696c0d78c829276fcc01a
Alexander Tuzhik <alprog@hotmail.com>
2024-11-03 21:32:45 +0100

    clean up

--- IxxtensionPackage.cs
+++ IxxtensionPackage.cs
@@ -1,25 +1,21 @@
-﻿using System;
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Community.VisualStudio.Toolkit;
-using Microsoft.VisualStudio;
-using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using Task = System.Threading.Tasks.Task;
 
 namespace Ixxtension
 {
-	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-	[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-	[ProvideMenuResource("Menus.ctmenu", 1)]
-	[Guid(PackageGuids.IxxtensionString)]
-	[ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
-	[ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
-	public sealed class IxxtensionPackage : ToolkitPackage
-	{
-		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
-		{
-			await this.RegisterCommandsAsync();
-		}
-	}
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.IxxtensionString)]
+    public sealed class IxxtensionPackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
 }
